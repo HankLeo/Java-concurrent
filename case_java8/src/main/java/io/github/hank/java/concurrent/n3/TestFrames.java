9b6b4c82dd641f8a363aa057e50ca585e17f9266
Hank <hao.liu@microfocus.com>
2020-05-12 17:38:52 +0800

    test thread frames

--- case_java8/src/main/java/io/github/hank/java/concurrent/n3/TestFrames.java
+++ case_java8/src/main/java/io/github/hank/java/concurrent/n3/TestFrames.java
@@ -1,13 +1,11 @@
 package io.github.hank.java.concurrent.n3;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j(topic = "c.TestFrames")
 public class TestFrames {
     public static void main(String[] args) {
-        Thread t1 = new Thread(){
-            @Override
-            public void run() {
-                method1(20);
-            }
-        };
+        Thread t1 = new Thread(() -> method1(20));
         t1.setName("t1");
         t1.start();
         method1(10);
@@ -17,10 +15,12 @@ public class TestFrames {
         int y = x + 1;
         Object m = method2();
         System.out.println(m);
+        log.debug("Thread {} is running", Thread.currentThread().getName());
     }
 
     private static Object method2() {
         Object n = new Object();
+        log.debug("Thread {} is running", Thread.currentThread().getName());
         return n;
     }
 }
