a466d59b71b31326e272aa7ed10b8b4821d3dcd4
Hank <hao.liu@microfocus.com>
2020-05-17 13:56:18 +0800

    线程的两阶段中止 Thread interrupt & yield

--- case_java8/src/main/java/io/github/hank/java/concurrent/n3/TestInterrupt.java
+++ case_java8/src/main/java/io/github/hank/java/concurrent/n3/TestInterrupt.java
@@ -10,12 +10,12 @@ import static io.github.hank.java.concurrent.n2.util.Sleeper.sleep;
 @Slf4j(topic = "c.TestInterrupt")
 public class TestInterrupt {
     public static void main(String[] args) throws InterruptedException {
-        test1();
+        test2();
     }
     private static void test4() {
         Thread t1 = new Thread(() -> {
             for (int i = 0; i < 5; i++) {
-                log.debug("park...");
+                log.debug("park...");  // for循环可以在下个迭代中再次park
                 LockSupport.park();
                 log.debug("打断状态：{}", Thread.interrupted());
             }
@@ -32,6 +32,9 @@ public class TestInterrupt {
             LockSupport.park();
             log.debug("unpark...");
             log.debug("打断状态：{}", Thread.currentThread().isInterrupted());
+
+            LockSupport.park();
+            log.debug("cannot be parked again...");
         }, "t1");
         t1.start();
 
@@ -44,25 +47,27 @@ public class TestInterrupt {
             while(true) {
                 Thread current = Thread.currentThread();
                 boolean interrupted = current.isInterrupted();
+                log.debug(" Thread.interrupted: {}", Thread.interrupted());
                 if(interrupted) {
-                    log.debug(" 打断状态: {}", interrupted);
+                    log.debug(" 打断状态: {}", true);
                     break;
                 }
             }
         }, "t2");
         t2.start();
 
-        Sleeper.sleep(0.5);
+        Sleeper.sleep(0.1);
         t2.interrupt();
     }
     private static void test1() throws InterruptedException {
         Thread t1 = new Thread(()->{
             Sleeper.sleep(1);
+            log.debug(" Thread.interrupted: {}", Thread.interrupted());
         }, "t1");
         t1.start();
 
         Sleeper.sleep(0.5);
         t1.interrupt();
-        log.debug(" 打断状态: {}", t1.isInterrupted());
+        log.debug(" 打断状态: {}", t1.isInterrupted());  // isInterrupted vs interrupted when thread state is sleep
     }
 }
--- case_java8/src/main/java/io/github/hank/java/concurrent/n3/TestYield.java
+++ case_java8/src/main/java/io/github/hank/java/concurrent/n3/TestYield.java
@@ -14,14 +14,14 @@ public class TestYield {
         Runnable task2 = () -> {
             int count = 0;
             for (;;) {
-//                Thread.yield();
+                Thread.yield();
                 System.out.println("              ---->2 " + count++);
             }
         };
         Thread t1 = new Thread(task1, "t1");
         Thread t2 = new Thread(task2, "t2");
-        t1.setPriority(Thread.MIN_PRIORITY);
-        t2.setPriority(Thread.MAX_PRIORITY);
+        //t1.setPriority(Thread.MIN_PRIORITY);
+        //t2.setPriority(Thread.MAX_PRIORITY);
         t1.start();
         t2.start();
     }
